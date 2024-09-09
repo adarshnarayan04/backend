88f54676cf02badf9e81a9f6fbceed1bba143561
adarshnarayan04 <adarshnarayangumla@gmail.com>
2024-09-09 20:35:43 +0530

    new file:   .gitignore

---
+++ 01basic/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()//to access env file
+
+//import 'dotenv/config'//new style--> add this("type": "module" in package.json)
+const express = require('express')//old style
+const app = express()
+const port = 4000
+
+//always use / in path
+app.get('/', (req, res) => {
+    //send the response
+  res.send('Hello World!')
+})
+//res : response
+//req: request
+
+//put / always in path
+app.get('/twitter', (req, res) => {
+    res.send('on twitter')
+    });
+
+    // to use env file
+
+    //process.env.variable_name ( name should be same as declared in env file)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+})
+//it will listen on port 3000
+
+//if we write a new file(then we have to restart the server) ---> relaoding not hapeen automatically(like in vite)
+//can use nodemon to restart the server automatically
