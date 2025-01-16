ad8b68d92c02848595e10a42265d7a07fd2792a1
Adarsh Narayan <adarshnarayangumla@gmail.com>
2025-01-17 02:44:19 +0530

    Clarify comment for JSON data handling in app.js

--- chai-backend-main/src/app.js
+++ chai-backend-main/src/app.js
@@ -15,7 +15,7 @@ app.use(
 
 //have to write this 4 line code(to recieve data form vaious thing(like forn ,json,image,url))
 
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "16kb" }));//so that we can recieve json data(  ex: in post request req.body generally cointain json data  --> will give undefined if we dont use this)
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //url encoder
 app.use(express.static("public"));
 //pulbic folder is used to store images pdf,and files that are used
