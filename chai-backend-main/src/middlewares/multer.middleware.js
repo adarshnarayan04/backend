21f194cf432c6bfcc8e5f1b2a18db3741d76abf6
Adarsh Narayan <adarshnarayangumla@gmail.com>
2025-03-27 13:12:22 +0530

    Enhance comments in multer middleware to clarify file upload handling and usage examples

--- chai-backend-main/src/middlewares/multer.middleware.js
+++ chai-backend-main/src/middlewares/multer.middleware.js
@@ -22,6 +22,26 @@ const storage = multer.diskStorage({
 export const upload = multer({
   storage, //storage : storage as we are usign es6 so we can write it as storage only (it will be converted to storage: storage)
 });
+//see user.route.js , if want to see how to upload
+
+//fieldname --> is the name given to input button which recieve file
+//<input type="file" name="profileImage"> ---> fieldname is profileImage
+// .single(fieldname)
+// Accept a single file with the name fieldname. The single file will be stored in req.file.
+
+// .array(fieldname[, maxCount])
+// upload.array("images", 5) //fieldname is "images", maxCount is 5
+// Accept an array of files, all with the name fieldname. Optionally error out if more than maxCount files are uploaded. The array of files will be stored in req.files.
+
+// .fields(fields)
+// Accept a mix of files, specified by fields. An object with arrays of files will be stored in req.files.
+
+// fields should be an array of objects with name and optionally a maxCount. Example:
+
+// [
+//   { name: 'avatar', maxCount: 1 },
+//   { name: 'gallery', maxCount: 8 }
+// ]
 
 //if want to throw error
 
